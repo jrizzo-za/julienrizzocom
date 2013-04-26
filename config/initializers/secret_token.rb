b7b24031ed42500dbd69a0063842c2e09a95b81a
jrizzo-za <julien@yola.com>
2013-04-27 00:22:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Julienrizzocom::Application.config.secret_token = 'd83dfb30e89a0c31ea483401342f527c05269281a6822cb81ca45213048e27425ee938120322278980c139e58530ebaea798cafc496cac269cf39d84f30ad2b5'
