a8770df467a512625fad638f5fefdb57731bbf74
jrizzo-za <julien@yola.com>
2013-04-28 14:44:22 +0200

    Override stylesheet generator to use LESS instead of SASS

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Julienrizzocom
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Use LESS in stylesheet generation instead of SASS
+    config.app_generators.stylesheet_engine :less
   end
 end
